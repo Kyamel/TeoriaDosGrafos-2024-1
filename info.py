b4cdd3f8f0f6afe950a33f854f7e25a96dfad7d8
Samuel Brito <samuelbrito@ufop.edu.br>
2024-07-15 14:05:01 -0300

    Aula 12/07

--- info.py
+++ info.py
@@ -1,15 +1,34 @@
+import listaAdjacencias
+import matrizAdjacencias
+
 # retorna a densidade do grafo:
 def densidade(grafo):
-    print("Implementar...")
+    return grafo.numArestas / (grafo.numVertices * (grafo.numVertices - 1))
 
 # retorna o complemento do grafo:
-def complemento(matAdj):
-    print("Implementar...")
+def complemento(grafo):
+    complemento = listaAdjacencias.ListaAdjacencias(grafo.numVertices)
+    # complemento = matrizAdjacencias.MatrizAdjacencias(grafo.numVertices)
+
+    for i in range(grafo.numVertices):
+        for j in range(grafo.numVertices):
+            if i != j and not grafo.possuiAresta(i, j):
+                complemento.addAresta(i, j)
+    
+    return complemento
 
 # retorna True se o grafo eh completo:
 def completo(grafo):
-    print("Implementar...")
+    for i in range(grafo.numVertices):
+        for j in range(grafo.numVertices):
+            if i != j and not grafo.possuiAresta(i, j):
+                return False
+    return True
 
 # retorna True se o grafo eh regular:
-def regular(matrizAdj):
-    print("Implementar...")
+def regular(grafo):
+    grau = grafo.grau(0)
+    for i in range(1, grafo.numVertices):
+        if grau != grafo.grau(i):
+            return False
+    return True
--- main.py
+++ main.py
@@ -1,28 +1,49 @@
 import matrizAdjacencias
 import listaAdjacencias
 import info
+import sys
 
-# criando um grafo:
-# grafo = listaAdjacencias.ListaAdjacencias(6)
-grafo = matrizAdjacencias.MatrizAdjacencias(6)
+# cria um grafo a partir de um arquivo:
+def leitura(nomeArquivo):
+    arquivo = open(nomeArquivo)
 
+    str = arquivo.readline()
+    str = str.split(" ")
+    numVertices = int(str[0])
+    numArestas = int(str[1])
 
-# adicionando arestas:
-grafo.addAresta(0,1)
-grafo.addAresta(0,2)
-grafo.addAresta(0,4)
-grafo.addAresta(3,0)
-grafo.addAresta(1, 4, 10)
+    grafo = listaAdjacencias.ListaAdjacencias(numVertices)
+    # grafo = matrizAdjacencias.MatrizAdjacencias(numVertices)
 
-# exibindo as listas de adjacencias:
-grafo.printGrafo()
+    for i in range(numArestas):
+        str = arquivo.readline()
+        str = str.split(" ")
+        origem = int(str[0])
+        destino = int(str[1])
+        peso = int(str[2])
+        grafo.addAresta(origem, destino, peso)
 
-# exibindo os vertices vizinhos de um determinado vertice:
-print("Vizinhos de {}: {}".format(0, grafo.vizinhos(0)))
-print("Vizinhos de {}: {}".format(1, grafo.vizinhos(1)))
+    return grafo
 
-# exibindo o grau de um vertice:
-print("Grau do vertice {}: {}".format(0, grafo.grau(0)))
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Numero invalido de parametros! Argumentos esperados: main.py grafo.txt")
+        sys.exit(1)
 
-# testando se existe uma aresta (1, 2) no grafo:
-print("Existe aresta (1, 2) no grafo? {}".format(grafo.possuiAresta(1, 2)));
+    # sys.argv[1] contem o nome do arquivo a ser lido
+    grafo = leitura(sys.argv[1])
+
+    print(info.densidade(grafo))
+
+    print("Grafo original:")
+    grafo.printGrafo()
+    print()
+
+    complemento = info.complemento(grafo)
+    print("Complemento:")
+    complemento.printGrafo()
+    print()
+
+    print(f'Grafo eh completo? {info.completo(grafo)}')
+
+    print(f'Grafo eh regular? {info.regular(grafo)}')
