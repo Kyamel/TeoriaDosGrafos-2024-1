e43a0f0018129aa2167a2185f8c5e8ac36885540
Kyamel <129009324+Kyamel@users.noreply.github.com>
2024-09-13 02:51:55 -0300

    feat: adicionado caminhoMinFloydWarshall

--- caminhoMinimo.py
+++ caminhoMinimo.py
@@ -12,20 +12,20 @@ def caminhoMinDijkstra(grafo: ListaAdjacencias, origem: int, destino: int) -> Tu
         - custo (int): O custo total do caminho mínimo.
         - tempo (float): O tempo de execução do algoritmo em segundos.
     """
-    
+
     start = time.time()
-    
+
     dist = [float('inf')] * grafo.numVertices # float('inf') é uma forma de representar infinito em Python
     prev = [None] * grafo.numVertices
-    
+
     dist[origem] = 0
     prev[origem] = origem
-    
+
     O = set(range(grafo.numVertices)) # set é uma forma de representar conjuntos em Python
     C = set()
 
     while C != set(range(grafo.numVertices)):
-        
+
         menor = float('inf')
         u = None
         for v in O:
@@ -47,7 +47,7 @@ def caminhoMinDijkstra(grafo: ListaAdjacencias, origem: int, destino: int) -> Tu
                     prev[v] = u
 
     end = time.time()
-    
+
     # Construindo o caminho do destino até a origem
     caminho = []
     atual = destino
@@ -61,9 +61,6 @@ def caminhoMinDijkstra(grafo: ListaAdjacencias, origem: int, destino: int) -> Tu
     caminho.reverse()
     
     return caminho, dist[destino], end - start
-
-def caminhoMinFloydWarshall(grafo: ListaAdjacencias, origem: int, destino: int) -> Tuple[List[int], int, float]:
-    pass
 
 def caminhoMinBellmanFord(grafo: ListaAdjacencias, origem: int, destino: int) -> Tuple[List[int], int, float]:
     """
@@ -93,14 +90,14 @@ def caminhoMinBellmanFord(grafo: ListaAdjacencias, origem: int, destino: int) ->
                     atualizou = True
         if not atualizou:
             break
-    
+
     # Verificação de ciclos negativos
     for u in range(grafo.numVertices):
         for v, peso in grafo.vizinhos(u):
             if dist[u] != float('inf') and dist[u] + peso < dist[v]:
                 end = time.time()
                 return None, float('-inf'), end - start
-            
+
     end = time.time()
 
     caminho = []
@@ -111,8 +108,64 @@ def caminhoMinBellmanFord(grafo: ListaAdjacencias, origem: int, destino: int) ->
         atual = prev[atual]
         if atual is None:
             return None, float('inf'), end - start
-    
+
     caminho.append(origem)
     caminho.reverse()
 
     return caminho, dist[destino], end - start
+
+def caminhoMinFloydWarshall(grafo: ListaAdjacencias, origem: int, destino: int) -> Tuple[List[int], int, float]:
+    """
+    Calcula o caminho mínimo do vértice de origem até o vértice de destino usando o algoritmo de Floyd-Warshall.
+
+    Returns:
+        - caminho (List[int]): O caminho da origem até o destino no formato de uma lista de inteiros representando cada vértice.
+        - custo (int): O custo total do caminho mínimo.
+        - tempo (float): O tempo de execução do algoritmo em segundos.
+    """
+
+    start = time.time()
+
+    dist = [[float('inf')] * grafo.numVertices for _ in range(grafo.numVertices)]
+    prev = [[None] * grafo.numVertices for _ in range(grafo.numVertices)]
+
+    for i in range(grafo.numVertices):
+        for j in range(grafo.numVertices):
+            if i == j:
+                dist[i][j] = 0
+                prev[i][j] = i
+            else:
+                for (v, peso) in grafo.vizinhos(i):
+                    if v == j:  # v == j, existe aresta (i, j)
+                        dist[i][j] = peso
+                        prev[i][j] = i
+                        break
+
+    # Algoritmo principal
+    for k in range(grafo.numVertices):
+        for i in range(grafo.numVertices):
+            for j in range(grafo.numVertices):
+                if dist[i][j] > dist[i][k] + dist[k][j]:
+                    dist[i][j] = dist[i][k] + dist[k][j]
+                    prev[i][j] = prev[k][j]
+
+    end = time.time()
+    caminho = []
+
+    # Verificação de ciclos negativos
+    if dist[origem][destino] == float('inf'):
+        return None, float('-inf'), end - start
+
+    # Construindo o caminho do destino até a origem
+    else:
+        atual = destino
+        while atual != origem:
+            caminho.append(atual)
+            atual = prev[origem][atual]
+            if atual is None:
+                return None, float('inf'), end - start
+
+        caminho.append(origem)
+        caminho.reverse()
+
+    return caminho, dist[origem][destino], end - start
--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import caminhoMinimo
-import matrizAdjacencias
 import listaAdjacencias
-import info
 import sys
-import busca
+import concurrent.futures
 
 # cria um grafo a partir de um arquivo:
 def leitura(nomeArquivo):
@@ -27,6 +25,15 @@ def leitura(nomeArquivo):
 
     return grafo
 
+TIME_LIMIT = 600 # 10 min
+def run_with_timeout(func, *args, timeout=TIME_LIMIT):
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        future = executor.submit(func, *args)  # Submete a função
+        try:
+            return future.result(timeout=timeout)  # Espera o resultado
+        except concurrent.futures.TimeoutError:
+            return None, None, TIME_LIMIT  # Se ultrapassar o tempo, retorna valores padrão
+
 if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Numero invalido de parametros! Argumentos esperados: main.py grafo.txt origem(int) destino(int)")
@@ -34,18 +41,18 @@ if __name__ == "__main__":
 
     # sys.argv[1] contem o nome do arquivo a ser lido
     grafo = leitura(sys.argv[1])
-    
+
     print("Processando...")
     print(50 * "-")
-    
+
     algoritmos = {
         "Dijkstra": caminhoMinimo.caminhoMinDijkstra,
         "BellmanFord": caminhoMinimo.caminhoMinBellmanFord,
-        #"FloydWarshall": caminhoMinimo.caminhoMinFloydWarshall
+        "FloydWarshall": caminhoMinimo.caminhoMinFloydWarshall,
     }
 
     for nome, func in algoritmos.items():
-        caminho, custo, tempo = func(grafo, int(sys.argv[2]), int(sys.argv[3]))
+        caminho, custo, tempo = run_with_timeout(func, grafo, int(sys.argv[2]), int(sys.argv[3]))
         print(f"Algorítimo:", nome)
         print("Caminho mínimo:", caminho)
         print("Custo:", custo)
